34320e6af22312769585930d854ff65aaa2c959f
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-20 14:07:10 +0500

    Add Program Files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Doviz
+{
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("USD to UZS");
+        System.Console.Write("Enter the amount of USD:  ");
+        decimal USD = Convert.ToDecimal(Console.ReadLine());
+        System.Console.WriteLine("CONVERTING...");
+        decimal UZS = USD*11630;
+        System.Console.WriteLine(UZS);
+    }
+}
+}
