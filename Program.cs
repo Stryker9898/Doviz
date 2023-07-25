52263695386cddd98710f71940b3873200888a75
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-25 13:44:03 +0500

    Add Switch Statement

--- Program.cs
+++ Program.cs
@@ -1,38 +1,43 @@
 ﻿using System;
 namespace Doviz
 {
-class Program
-{
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Choose one of the numbers");
+        static void Main()
+        {
+            Console.WriteLine("Choose one of the numbers");
             Console.WriteLine("1. USD -> UZS");
             Console.WriteLine("2. UZS -> USD");
 
-             int number = Convert.ToInt32(Console.ReadLine());
-            
-            if(number == 1)
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
             {
-              System.Console.WriteLine("USD to UZS");
-        System.Console.Write("Enter the amount of USD:  ");
-        decimal USD = Convert.ToDecimal(Console.ReadLine());
-        System.Console.WriteLine("CONVERTING...");
-        decimal UZS = USD*11630;
-        System.Console.WriteLine(UZS);
+                case 1:
+                    {
+                        System.Console.WriteLine("USD to UZS");
+                        System.Console.Write("Enter the amount of USD:  ");
+                        decimal USD = Convert.ToDecimal(Console.ReadLine());
+                        System.Console.WriteLine("CONVERTING...");
+                        decimal UZS = USD * 11630;
+                        System.Console.WriteLine(UZS);
+                        break;
+                    }
+                case 2:
+                    {
+                        System.Console.WriteLine("USD to UZS");
+                        System.Console.Write("Enter the amount of UZS:  ");
+                        decimal UZS = Convert.ToDecimal(Console.ReadLine());
+                        System.Console.WriteLine("CONVERTING...");
+                        decimal USD = UZS / 11630;
+                        System.Console.WriteLine(USD);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
             }
 
-            else if(number == 2)
-            {
-                System.Console.WriteLine("USD to UZS");
-        System.Console.Write("Enter the amount of UZS:  ");
-        decimal UZS = Convert.ToDecimal(Console.ReadLine());
-        System.Console.WriteLine("CONVERTING...");
-        decimal USD = UZS/11630;
-        System.Console.WriteLine(USD);
-            }
 
-            else
-               Console.WriteLine("Invalid input");
+        }
     }
-}
 }
